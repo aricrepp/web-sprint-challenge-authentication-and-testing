6af421d447b7904d96e205d150c4e7d73a0ec09f
Aric Repp <aricmrepp@gmail.com>
2020-09-11 19:53:34 -0400

    register/login working with cookies

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,69 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Users = require('./auth-model');
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res, next) => {
   // implement registration
+
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({
+        message: 'Username taken',
+      });
+    }
+
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 14),
+    });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res, next) => {
   // implement login
+
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid Credentials',
+      });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: 'Invalid Credentials',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userID: user.id,
+        userRole: 'admin',
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie('token', token);
+
+    res.json({
+      message: `Welcome ${user.username}!`,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
