897f08b3ff0dc58e670dd97339bf08c3a47cf011
Aric Repp <aricmrepp@gmail.com>
2020-09-11 22:02:08 -0400

    uncomment tests checked

--- __tests__/jokes.js
+++ __tests__/jokes.js
@@ -11,13 +11,13 @@ afterAll(async () => {
 });
 
 describe('testing users', () => {
-  //   it('GET jokes error', async () => {
-  //     const res = await supertest(server).get('/api/jokes');
-  //     expect(res.statusCode).toBe(401);
-  //     expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
-  //     console.log(res.body);
-  //     expect(res.body).toEqual({ you: 'shall not pass!' });
-  //   });
+  it('GET jokes error', async () => {
+    const res = await supertest(server).get('/api/jokes');
+    expect(res.statusCode).toBe(401);
+    expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
+    console.log(res.body);
+    expect(res.body).toEqual({ you: 'shall not pass!' });
+  });
 
   it('GET jokes with token', async () => {
     await supertest(server).post('/api/auth/register').send({
